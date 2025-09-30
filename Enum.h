3f23d490a5131474f83156c8e86406adb478c920
TToJlkoBHuK <28arhalex@gmail.com>
2025-09-30 23:07:16 +0300

    base class
    basic enumeration of objects and characteristics

---
+++ Enum.h
@@ -0,0 +1,35 @@
+#pragma once
+
+//Character
+enum class CharacterClass {
+	ROUGE,
+	WARRIOR,
+	BARBARIAN
+};
+
+//weapon
+enum class WeaponType {
+	DAGGER,
+	SWORD,
+	CLUB,
+	AXE,
+	SPEAR,
+	LEGENDARY_SWORD
+};
+
+//damage
+enum class DamageType {
+	PIERCING,
+	SLASHING,
+	BLUDGEONING
+};
+
+//Monster
+enum class MonsterType {
+	GOBLIN,
+	SKELETEN,
+	SLIME,
+	GHOST,
+	GLOEM,
+	DRAGON
+};
