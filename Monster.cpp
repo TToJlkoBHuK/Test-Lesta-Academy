74bc3b07a745de3988bc48c9aa0af043b209a035
TToJlkoBHuK <28arhalex@gmail.com>
2025-10-08 16:13:43 +0300

    update monster
    update for new classes

--- Monster.cpp
+++ Monster.cpp
@@ -1,77 +1,89 @@
 #include "Monster.h"
+#include "Utils.h"
+#include "TypeRegistry.h"
 
-Monster::Monster(MonsterType type) : monsterType(type) {
-	switch (type) {
-		case MonsterType::GOBLIN:
-			name = "Goblin"; hp = 5; maxhp = 5; strength = 1; dexterity = 1; endurance = 1; 
-			currentWeapon = { "rusty knife", 2, DamageType::PIERCING, WeaponType::DAGGER };
-			loot = { "dagger", 2, DamageType::PIERCING, WeaponType::DAGGER };
-			break;
-		case MonsterType::SKELETEN:
-			name = "Skeleten"; hp = 10; maxhp = 10; strength = 2; dexterity = 2; endurance = 1;
-			currentWeapon = { "bone sword", 2, DamageType::SLASHING, WeaponType::SWORD };
-			loot = { "club", 3, DamageType::BLUDGEONING, WeaponType::CLUB };
-			break;
-		case MonsterType::SLIME:
-			name = "Slime"; hp = 8; maxhp = 8; strength = 3; dexterity = 1; endurance = 2;
-			currentWeapon = { "acid spit", 1, DamageType::PIERCING, WeaponType::SPEAR };
-			loot = { "acid spit", 3, DamageType::PIERCING, WeaponType::SPEAR };
-			break;
-		case MonsterType::GHOST:
-			name = "Ghost"; hp = 6; maxhp = 6; strength = 1; dexterity = 3; endurance = 1;
-			currentWeapon = { "touch of horror", 3, DamageType::SLASHING, WeaponType::SWORD };
-			loot = { "sword", 3, DamageType::SLASHING, WeaponType::SWORD };
-			break;
-		case MonsterType::GOLEM:
-			name = "Golem"; hp = 10; maxhp = 10; strength = 3; dexterity = 1; endurance = 3;
-			currentWeapon = { "stone fist", 1, DamageType::BLUDGEONING, WeaponType::CLUB };
-			loot = { "axe", 4, DamageType::SLASHING, WeaponType::AXE };
-			break;
-		case MonsterType::DRAGON:
-			name = "Dragon"; hp = 20; maxhp = 20; strength = 3; dexterity = 3; endurance = 3;
-			currentWeapon = { "bite", 4, DamageType::PIERCING, WeaponType::SPEAR };
-			loot = { "legendary sword", 10, DamageType::SLASHING, WeaponType::LEGENDARY_SWORD };
-			break;
-	}
+void Monster::takeModeDamage(int incomingDamage, Obj_game& attaker) {
+	takeDamage(incomingDamage);
 }
 
-int Monster::calcBaseDamage() {
-	return currentWeapon.damage + strength;
+Goblin::Goblin() {
+	name = "Goblin"; hp = 5; maxhp = 5; strength = 1; dexterity = 1; endurance = 1;
+	currentWeapon = { "rusty knife", 2, &TypeRegisrtry::getPiercing(), &TypeRegisrtry::getDagger()};
+	loot = { "dagger", 2, &TypeRegisrtry::getPiercing(), &TypeRegisrtry::getDagger()};
 }
 
-int Monster::applyDamageMode(int baseDmg, int turn) {
-	int modDmg = baseDmg;
-
-	//Ghost
-	if (monsterType == MonsterType::GHOST) {
-		modDmg += 1;
-	}
-
-	//dragon
-	if (monsterType == MonsterType::DRAGON && turn % 3 == 0) {
-		modDmg += 3;
-	}
-	return modDmg;
+Skeleton::Skeleton() {
+	name = "Skeleten"; hp = 10; maxhp = 10; strength = 2; dexterity = 2; endurance = 1;
+	currentWeapon = { "bone sword", 2, &TypeRegisrtry::getSlashing(), &TypeRegisrtry::getSword() };
+	loot = { "club", 3, &TypeRegisrtry::getBludgeoning(), &TypeRegisrtry::getClub() };
 }
 
-void Monster::takeModeDamage(int incomingDmg, Obj_game& attacker) {
-	int finalDmg = incomingDmg;
-	DamageType attacktype = attacker.getweapon().damage_type;
+Slime::Slime() {
+	name = "Slime"; hp = 8; maxhp = 8; strength = 3; dexterity = 1; endurance = 2;
+	currentWeapon = { "acid spit", 1, &TypeRegisrtry::getPiercing(), &TypeRegisrtry::getSpear() };
+	loot = { "acid spit", 3, &TypeRegisrtry::getPiercing(), &TypeRegisrtry::getSpear() };
+}
 
-	//skeleten
-	if (monsterType == MonsterType::SKELETEN && attacktype == DamageType::BLUDGEONING) {
+Ghost::Ghost() {
+	name = "Ghost"; hp = 6; maxhp = 6; strength = 1; dexterity = 3; endurance = 1;
+	currentWeapon = { "touch of horror", 3, &TypeRegisrtry::getSlashing(), &TypeRegisrtry::getSword() };
+	loot = { "sword", 3, &TypeRegisrtry::getSlashing(), &TypeRegisrtry::getSword() };
+}
+
+Golem::Golem() {
+	name = "Golem"; hp = 10; maxhp = 10; strength = 3; dexterity = 1; endurance = 3;
+	currentWeapon = { "stone fist", 1, &TypeRegisrtry::getBludgeoning(), &TypeRegisrtry::getClub() };
+	loot = { "axe", 4, &TypeRegisrtry::getSlashing(), &TypeRegisrtry::getAxe() };
+}
+
+Dragon::Dragon() {
+	name = "Dragon"; hp = 20; maxhp = 20; strength = 3; dexterity = 3; endurance = 3;
+	currentWeapon = { "bite", 4, &TypeRegisrtry::getPiercing(), &TypeRegisrtry::getSpear() };
+	loot = { "legendary sword", 10, &TypeRegisrtry::getSlashing(), &TypeRegisrtry::getLegendarySword() };
+}
+
+void Skeleton::takeModeDamage(int incomingDamage, Obj_game& attaker) {
+	int finalDmg = incomingDamage;
+	if (attaker.getweapon().damage_type == &TypeRegisrtry::getBludgeoning()) {
 		finalDmg *= 2;
 	}
-
-	//slime
-	if (monsterType == MonsterType::SLIME && attacktype == DamageType::SLASHING) {
-		finalDmg = strength;
-	}
-
-	//golem
-	if (monsterType == MonsterType::GOLEM) {
-		finalDmg -= endurance;
-	}
-
 	takeDamage(finalDmg);
 }
+
+void Slime::takeModeDamage(int incomingDamage, Obj_game& attaker) {
+	int finalDmg = incomingDamage;
+	if (attaker.getweapon().damage_type == &TypeRegisrtry::getSlashing()) {
+		finalDmg *= strength;
+	}
+	takeDamage(finalDmg);
+}
+
+int Ghost::applyDamageMode(int baseDamage, int turn) {
+	return baseDamage + 1;
+}
+
+void Golem::takeModeDamage(int incomingDamage, Obj_game& attaker) {
+	takeDamage(incomingDamage - endurance);
+}
+
+int Dragon::applyDamageMode(int baseDamage, int turn) {
+	if (turn % 3 == 0) {
+		return baseDamage + 3;
+	}
+	return baseDamage;
+}
+
+std::unique_ptr<Monster> MonsterFactory::createRandomMonster() {
+	int type = getRandom(0, 5);
+	switch (type) {
+		case 0: return std::make_unique<Goblin>();
+		case 1: return std::make_unique<Skeleton>();
+		case 2: return std::make_unique<Slime>();
+		case 3: return std::make_unique<Ghost>();
+		case 4: return std::make_unique<Golem>();
+		case 5: return std::make_unique<Dragon>();
+		default: return std::make_unique<Goblin>();
+	}
+}
+
+
--- Monster.h
+++ Monster.h
@@ -1,17 +1,59 @@
 #pragma once
 #include "obj_game.h"
+#include "TypeRegistry.h"
+
+
 
 class Monster : public Obj_game {
 public:
-	Monster(MonsterType type);
+	virtual ~Monster() = default;
 	Weapon getloot() const { return loot; }
 
 	//fight logical
-	int calcBaseDamage();
-	int applyDamageMode(int baseDamage, int turn);
-	void takeModeDamage(int incomingDamage, Obj_game& attack);
+	virtual int calcBaseDamage() { return currentWeapon.damage + strength; }
+	virtual int applyDamageMode(int baseDamage, int turn) { return baseDamage; }
+	virtual void takeModeDamage(int incomingDamage, Obj_game& attack);
 
-private:
-	MonsterType monsterType;
+protected:
 	Weapon loot;
+};
+
+class MonsterFactory {
+public:
+	static std::unique_ptr<Monster> createRandomMonster();
+};
+
+class Goblin : public Monster {
+public:
+	Goblin();
+};
+
+class Skeleton : public Monster {
+public:
+	Skeleton();
+	void takeModeDamage(int incomingDamage, Obj_game& attaker) override;
+};
+
+class Slime : public Monster {
+public:
+	Slime();
+	void takeModeDamage(int incomingDamage, Obj_game& attaker) override;
+};
+
+class Ghost : public Monster {
+public:
+	Ghost();
+	int applyDamageMode(int baseDamage, int turn) override;
+};
+
+class Golem : public Monster {
+public:
+	Golem();
+	void takeModeDamage(int incomingDamage, Obj_game& attaker) override;
+};
+
+class Dragon : public Monster {
+public:
+	Dragon();
+	int applyDamageMode(int baseDamage, int turn) override;
 };
